8ae76bb06150ed5be98fda0dc15735f4a54f96d1
Mark Payne <ampayne@mit.edu>
2019-10-15 14:47:01 -0400

    Implement MILP scheme of Buerger et al. for error canceling reactions

--- arkane/isodesmicTest.py
+++ arkane/isodesmicTest.py
@@ -186,7 +186,9 @@ class TestErrorCancelingScheme(unittest.TestCase):
 
     def setUp(self):
         self.propene = ErrorCancelingSpecies(Molecule(smiles='CC=C'), (100, 'kJ/mol'), 'test')
+        self.propane = ErrorCancelingSpecies(Molecule(smiles='CCC'), (75, 'kJ/mol'), 'test')
         self.butane = ErrorCancelingSpecies(Molecule(smiles='CCCC'), (150, 'kJ/mol'), 'test')
+        self.butene = ErrorCancelingSpecies(Molecule(smiles='C=CCC'), (175, 'kJ/mol'), 'test')
         self.benzene = ErrorCancelingSpecies(Molecule(smiles='c1ccccc1'), (-50, 'kJ/mol'), 'test')
         self.caffeine = ErrorCancelingSpecies(Molecule(smiles='CN1C=NC2=C1C(=O)N(C(=O)N2C)C'), (300, 'kJ/mol'), 'test')
         self.ethyne = ErrorCancelingSpecies(Molecule(smiles='C#C'), (200, 'kJ/mol'), 'test')
@@ -199,6 +201,23 @@ class TestErrorCancelingScheme(unittest.TestCase):
         isodesmic_scheme = IsodesmicScheme(self.propene, [self.butane, self.benzene, self.caffeine, self.ethyne])
 
         self.assertEqual(isodesmic_scheme.reference_species, [self.butane, self.benzene])
+
+    def test_find_error_canceling_reaction(self):
+        """
+        Test that the MILP problem can be solved to find a single isodesmic reaction
+        """
+        scheme = IsodesmicScheme(self.propene, [self.propane, self.butane, self.butene, self.caffeine, self.ethyne])
+
+        # Note that caffeine and ethyne will not be allowed, so for the full set the indices are [0, 1, 2]
+        rxn = scheme._find_error_canceling_reaction([0, 1, 2], milp_software='lpsolve')
+        self.assertEqual(rxn.species[self.butane], -1)
+        self.assertEqual(rxn.species[self.propane], 1)
+        self.assertEqual(rxn.species[self.butene], 1)
+
+        rxn = scheme._find_error_canceling_reaction([0, 1, 2], milp_software='pyomo')
+        self.assertEqual(rxn.species[self.butane], -1)
+        self.assertEqual(rxn.species[self.propane], 1)
+        self.assertEqual(rxn.species[self.butene], 1)
 
 
 if __name__ == '__main__':
