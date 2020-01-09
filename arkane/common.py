5a4c028048925f2d6e7c2ea2f062dbca06f6625d
Mark Payne <ampayne@mit.edu>
2020-01-09 10:09:21 -0500

    Format ArkaneSpecies YAML so that identifiers and properties are grouped

--- arkane/common.py
+++ arkane/common.py
@@ -133,6 +133,10 @@ class ArkaneSpecies(RMGObject):
         self.RMG_version = RMG_version if RMG_version is not None else __version__
         self.datetime = datetime if datetime is not None else time.strftime("%Y-%m-%d %H:%M")
 
+        # Define which attributes are identifiers and which are properties
+        self._identifiers = ['smiles', 'adjacency_list', 'inchi', 'inchi_key', 'label', 'formula']
+        self._properties = ['molecular_weight', 'symmetry_number', 'charge', 'multiplicity']
+
     def __repr__(self):
         """
         Return a string representation that can be used to reconstruct the object
@@ -144,6 +148,25 @@ class ArkaneSpecies(RMGObject):
                 result += '{0!r}: {1!r}'.format(str(key), str(value))
         result += '}'
         return result
+
+    def as_dict(self):
+        dictionary = super(ArkaneSpecies, self).as_dict()
+        new_dictionary = {'Identifiers': {}, 'Properties': {}}
+
+        # Add additional formatting for identifiers, and properties
+        for attr, value in dictionary.items():
+            if attr in self._identifiers:
+                new_dictionary['Identifiers'].update({attr: value})
+            elif attr in self._properties:
+                new_dictionary['Properties'].update({attr: value})
+            else:
+                new_dictionary.update({attr: value})
+
+        return new_dictionary
+
+    def make_object(self, data, class_dict):
+        data = expand_formatted_blocks(data)
+        super(ArkaneSpecies, self).make_object(data, class_dict)
 
     def update_species_attributes(self, species=None):
         """
@@ -248,6 +271,7 @@ class ArkaneSpecies(RMGObject):
             content = f.read()
         content = replace_yaml_syntax(content, label)
         data = yaml.safe_load(stream=content)
+        data = expand_formatted_blocks(data)
         if label:
             # First, warn the user if the label doesn't match
             try:
@@ -326,6 +350,26 @@ def replace_yaml_syntax(content, label=None):
     if replaced_keys:
         logging.info('\n')
     return content
+
+
+def expand_formatted_blocks(data_dictionary):
+    """
+    Unpacks attributes contained in formatted blocks (if present) from an ArkaneSpecies dictionary obtained from a
+    YAML file
+    Args:
+        data_dictionary (dict): Dictionary representation of an ArkaneSpecies object
+
+    Returns:
+        dict: Dictionary representation of an ArkaneSpecies object with all formatted blocks properly unpacked
+
+    """
+    expandable_blocks = ['Identifiers', 'Properties']
+    new_data = {attr: value for attr, value in data_dictionary.items() if attr not in expandable_blocks}
+    for block in expandable_blocks:
+        if block in data_dictionary:
+            new_data.update(data_dictionary[block])
+
+    return new_data
 
 
 def is_pdep(job_list):
