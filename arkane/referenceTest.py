abb51490b43554b46afc436afad4beb3fa4934fa
Mark <ampayne@mit.edu>
2020-03-13 15:32:05 -0400

    Add ReferenceDataEntry class to standardize ReferenceSpecies input data

--- arkane/referenceTest.py
+++ arkane/referenceTest.py
@@ -35,8 +35,9 @@ This script contains unit tests of the :mod:`arkane.reference` module.
 import os
 import unittest
 
-from arkane.reference import ReferenceSpecies
+from arkane.reference import ReferenceSpecies, ReferenceDataEntry
 from rmgpy.species import Species
+from rmgpy.thermo import ThermoData
 
 
 ################################################################################
@@ -53,6 +54,8 @@ class TestReferenceSpecies(unittest.TestCase):
         cls.methane = Species(smiles='C')
         cls.ethane = Species(smiles='CC')
         cls.propane = Species(smiles='CCC')
+
+        cls.thermo_data = ThermoData(H298=(100.0, 'kJ/mol'), S298=(100.0, 'J/(mol*K)'))
 
     def test_instantiate_reference_species(self):
         """
@@ -96,6 +99,17 @@ class TestReferenceSpecies(unittest.TestCase):
 
         # Finally, delete this newly created file
         os.remove(load_path)
+
+    def test_reference_data_entry(self):
+        """
+        Test that the ReferenceDataEntry class functions properly and enforces the standard for storing data
+        """
+        data_entry = ReferenceDataEntry(self.thermo_data)
+        self.assertIsInstance(data_entry.thermo_data, ThermoData)
+        self.assertEqual(data_entry.thermo_data.H298.value_si, 100000.0)
+
+        with self.assertRaises(ValueError):
+            ReferenceDataEntry({'H298': (100.0, 'kJ/mol')})
 
 
 if __name__ == '__main__':
